4514cb84de89bd571786ba76979781f250dfe9ba
Anastasia Kozaeva <sacret190990@gmail.com>
2015-01-20 00:07:11 +0400

    fixes Jan 20

--- sites/all/themes/subtheme/template.php
+++ sites/all/themes/subtheme/template.php
@@ -4,3 +4,9 @@
  * @file
  * template.php
  */
+
+function subtheme_preprocess_image_style(&$vars) {
+  if (isset($vars['style_name']) && ($vars['style_name'] == 'object_300x200' || $vars['style_name'] == 'progs_image' || $vars['style_name'] ==  'gallery_image_full' || $vars['style_name'] ==  'portfolio_400_200')) {
+    $vars['attributes']['class'][] = 'img-responsive'; // can be 'img-rounded', 'img-circle', or 'img-thumbnail'
+  }
+}
